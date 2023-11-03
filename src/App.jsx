4f99fdfa6dca50b1f41bdfa40747595f907ade24
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-11-03 19:32:27 +0600

    set up

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  const routes = [
+    { name: "Home", path: "/", id: 1 },
+    { name: "About Us", path: "/about", id: 2 },
+    { name: "Services", path: "/services", id: 3 },
+    { name: "Contact Us", path: "/contact", id: 4 },
+  ];
+
+  return (
+    <>
+      <h1>Hello React</h1>
+    </>
+  );
+}
+
+export default App;
